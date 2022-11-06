1c9f7002a3220a370bbec1fd7bef53895c7da3d6
EC2 Default User <kazukipump7@gmail.com>
2022-11-06 15:11:15 +0000

    [Add]PHP

--- array.php
+++ array.php
@@ -21,6 +21,15 @@
 
     ?>
     
+       <?php
+      $scores = array('数学' => 70,'英語' => 90,'国語' => 80);
+
+      $scores['国語'] += 5;
+
+      echo $scores['国語'];
+
+    ?>
+    
     <?php
       for ($i = 51; $i => 100; $i++) {
         echo $i
